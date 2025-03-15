ba33f817ce96904cbc1ada6fee22d25c088f6dcd
Darcy <darcymcbeth@proton.me>
2025-03-15 12:26:10 +0000

    Could be ready to go. Needs to be tested on VM.

--- main.c
+++ main.c
@@ -19,17 +19,28 @@ int CountFiles(const char* path) {
     return count;
 }
 
-void openImages(int* totalImages, const char* path) {
-    for (int i = 0; i < *totalImages; i++) {
-        char buffer[256];
-        
+void openImages(int* count, const char* path) {
+    WIN32_FIND_DATAA findData;
+    HANDLE hFind = FindFirstFileA(path, &findData);
+    if (hFind == INVALID_HANDLE_VALUE) {
+        return;
     }
-
+    void *loadedGraphic = malloc(128 * 128);
+    do {
+        if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            continue;
+        }
+        char fullPath[256];
+        sprintf(fullPath, "%s%s", "./images/miku/", findData.cFileName);
+        printf("%s\n", fullPath);
+        FILE* loadedGraphic = fopen(fullPath, "r");
+    } while (FindNextFileA(hFind, &findData));
+    FindClose(hFind);
 }
 
 int main() {
     // Make our variables.
-    const char* images = "./images/*"; // Remember to use the *.
+    const char* images = "./images/miku/*"; // Remember to use the *.
     int countingFiles = CountFiles(images);
     printf("%d", countingFiles);
     openImages(&countingFiles, images);
